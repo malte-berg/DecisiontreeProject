e9d9e9ba7110a7768e0f4b38f0dd795b0e805467
Malte Berg <malte@malte.nu>
2025-04-02 10:29:55 +0200

    Implement basic Player class

---
+++ Game/Assets/Scenes/Combat/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Player : GameCharacter {
+
+    Combat c;
+
+    // STATS
+    int hp;
+    int vitality;
+    int armor;
+    int strength;
+    int magic;
+    int mana;
+    int maxMana;
+
+    // SKILL
+    public Skill[] skills;
+    int skillCount;
+    int selectedSkill = 0;
+
+    // INVENTORY
+    Equipment equipment;
+    Item[] inventory;
+
+    // PLAYER STATS
+    int gold;
+    int skillPoints;
+
+    public Player() : base(hp, vitality, armor, strength, magic, mana, maxMana, skills, skillCount, equipment, inventory){
+
+        int gold = 10;
+        int skillPoints = 0;
+
+    }
+    
+}
