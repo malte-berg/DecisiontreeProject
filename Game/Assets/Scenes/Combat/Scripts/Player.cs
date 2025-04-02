07e344caa34acbd4cc0e16f800bda0fa280e23bb
IsacOlofsson <isacol@kth.se>
2025-04-02 13:42:45 +0200

    o

--- Game/Assets/Scenes/Combat/Scripts/Player.cs
+++ Game/Assets/Scenes/Combat/Scripts/Player.cs
@@ -6,9 +6,9 @@ public class Player : GameCharacter {
     int gold;
     int skillPoints;
 
-    // public Player() : base(){
+    public Player() : base(){
 
-    // }
+    }
 
     public override void Init(Combat c){
 
@@ -16,8 +16,26 @@ public class Player : GameCharacter {
         equipment = gameObject.GetComponent<Equipment>();
         DontDestroyOnLoad(gameObject);
 
-    //     int gold = 10;
-    //     int skillPoints = 0;
+        int gold = 10;
+        int skillPoints = 0;
+
+        skills[0] = new Punch(this);
+
+        // OP dev privilege
+        inventory[0] = new Weapon("Excalibur", 9999, 10, 1.2f, 5, 1.1f, 224, 10.7f, 23, 1.2f, 162, 1.2f);
+        equipment.Equip(inventory[0]);
+
+    }
+
+    public void HidePlayer(){
+
+        transform.GetChild(0).gameObject.SetActive(false);
+
+    }
+
+    public void ShowPlayer(){
+
+        transform.GetChild(0).gameObject.SetActive(true);
 
     }
     
