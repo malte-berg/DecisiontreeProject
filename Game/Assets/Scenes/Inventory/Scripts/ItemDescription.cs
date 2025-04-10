1294aa8f98e6dc15b0aa4801b9dae9831094e568
Viktor Sandström <visand@kth.se>
2025-04-10 12:12:02 +0200

    Fixed inventory ItemDescription

--- Game/Assets/Scenes/Inventory/Scripts/ItemDescription.cs
+++ Game/Assets/Scenes/Inventory/Scripts/ItemDescription.cs
@@ -21,9 +21,9 @@ public class ItemDescription : MonoBehaviour{
         slide.transform.GetChild(2).GetComponent<TMP_Text>().text = target.Name;
         string description = "";
 
-        switch(target.GetType()){
+        switch(target){
 
-            case Type headType when headType == typeof(Head):
+            case Head:
                 Head head = target as Head;
                 description += $"<color=green>Vitality: \t+{head.VitalityAdd} (+{Mathf.RoundToInt(head.VitalityMult*100)-100}%)</color>\n";
                 description += $"<color=orange>Armor: \t+{head.ArmorAdd} (+{Mathf.RoundToInt(head.ArmorMult*100)-100}%)</color>\n";
@@ -31,7 +31,7 @@ public class ItemDescription : MonoBehaviour{
                 description += $"<color=blue>Magic: \t+{head.MagicAdd} (+{Mathf.RoundToInt(head.MagicMult*100)-100}%)</color>\n";
                 description += $"<color=purple>Mana: \t+{head.ManaAdd} (+{Mathf.RoundToInt(head.ManaMult*100)-100}%)</color>\n";
                 break;
-            case Type torsoType when torsoType == typeof(Torso):
+            case Torso:
                 Torso torso = target as Torso;
                 description += $"<color=green>Vitality: \t+{torso.VitalityAdd} (+{Mathf.RoundToInt(torso.VitalityMult*100)-100}%)</color>\n";
                 description += $"<color=orange>Armor: \t+{torso.ArmorAdd} (+{Mathf.RoundToInt(torso.ArmorMult*100)-100}%)</color>\n";
@@ -39,7 +39,7 @@ public class ItemDescription : MonoBehaviour{
                 description += $"<color=blue>Magic: \t+{torso.MagicAdd} (+{Mathf.RoundToInt(torso.MagicMult*100)-100}%)</color>\n";
                 description += $"<color=purple>Mana: \t+{torso.ManaAdd} (+{Mathf.RoundToInt(torso.ManaMult*100)-100}%)</color>\n";
                 break;
-            case Type bootsType when bootsType == typeof(Boots):
+            case Boots:
                 Boots boots = target as Boots;
                 description += $"<color=green>Vitality: \t+{boots.VitalityAdd} (+{Mathf.RoundToInt(boots.VitalityMult*100)-100}%)</color>\n";
                 description += $"<color=orange>Armor: \t+{boots.ArmorAdd} (+{Mathf.RoundToInt(boots.ArmorMult*100)-100}%)</color>\n";
@@ -47,7 +47,7 @@ public class ItemDescription : MonoBehaviour{
                 description += $"<color=blue>Magic: \t+{boots.MagicAdd} (+{Mathf.RoundToInt(boots.MagicMult*100)-100}%)</color>\n";
                 description += $"<color=purple>Mana: \t+{boots.ManaAdd} (+{Mathf.RoundToInt(boots.ManaMult*100)-100}%)</color>\n";
                 break;
-            case Type weaponType when weaponType == typeof(Weapon):
+            case Weapon:
                 Weapon weapon = target as Weapon;
                 description += $"<color=green>Vitality: \t+{weapon.VitalityAdd} (+{Mathf.RoundToInt(weapon.VitalityMult*100)-100}%)</color>\n";
                 description += $"<color=orange>Armor: \t+{weapon.ArmorAdd} (+{Mathf.RoundToInt(weapon.ArmorMult*100)-100}%)</color>\n";
@@ -55,9 +55,11 @@ public class ItemDescription : MonoBehaviour{
                 description += $"<color=blue>Magic: \t+{weapon.MagicAdd} (+{Mathf.RoundToInt(weapon.MagicMult*100)-100}%)</color>\n";
                 description += $"<color=purple>Mana: \t+{weapon.ManaAdd} (+{Mathf.RoundToInt(weapon.ManaMult*100)-100}%)</color>\n";
                 break;
-            case Type consumableType when consumableType == typeof(Consumable):
+            case Consumable:
+                // TODO
                 break;
             default:
+                Debug.LogError("No item type found");
                 break;
 
         }
