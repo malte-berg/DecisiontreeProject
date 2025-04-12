190c8f901298b7d4a9b54ce0d6a7befc9ae39322
Viktor Sandström <visand@kth.se>
2025-04-12 16:56:45 +0200

    Going to try a different way of solving this

--- Game/Assets/Scenes/Cutscene/CutsceneManager.cs
+++ Game/Assets/Scenes/Cutscene/CutsceneManager.cs
@@ -8,32 +8,59 @@ public class CutsceneManager : MonoBehaviour{
 
     void Start(){
 
+        StartCoroutine(Loading());
+
+    }
+
+    IEnumerator Loading(){
+
+        // Wait for previous scene to unload
+        print($"{SceneManager.GetSceneAt(0).name} is perhaps equal to: {SceneManager.GetActiveScene().name}");
+        while(SceneManager.GetSceneAt(0) != SceneManager.GetActiveScene()){
+            print($"{SceneManager.GetSceneAt(0).name} is perhaps equal to: {SceneManager.GetActiveScene().name}");
+            yield return null;
+        }
+
         int cs = -1;
         Player p = GameObject.Find("Player").GetComponent<Player>();
 
         if(p != null)
             cs = p.Cutscene;
 
-        if(cs == -1)
-            EndCutscene();
-        else
-            StartCoroutine(DoAnimations(cs));
+        if(cs != -1)
+            yield return StartCoroutine(DoAnimation(cs));
+        
+        StartCoroutine(EndCutscene());
 
     }
 
-    IEnumerator DoAnimations(int i){
+    IEnumerator DoAnimation(int i){
 
-        GameObject[] rootObjects = SceneManager.GetSceneAt(2).GetRootGameObjects();
+        GameObject[] rootObjects = SceneManager.GetSceneAt(1).GetRootGameObjects();
         foreach (GameObject obj in rootObjects) obj.SetActive(false);
         yield return StartCoroutine(sceneScripts[i].RunAnimation().GetEnumerator());
         foreach (GameObject obj in rootObjects) obj.SetActive(true);
-        EndCutscene();
 
     }
 
-    void EndCutscene(){
+    IEnumerator EndCutscene(){
 
-        SceneManager.UnloadSceneAsync("Cutscene");
+        string temp = "SCENES RN:\n";
+
+        for(int i = 0; true; i++){
+            try{
+                temp += $"[{i}] {SceneManager.GetSceneAt(i).name}\n";
+            } catch {
+                print(temp);
+                break;
+            }
+        }
+
+        while(!SceneManager.GetSceneAt(1).isLoaded)
+            yield return null;
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneAt(1));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(0));
 
     }
     
--- Game/Assets/Scenes/StartMenu/Scripts/SceneSwitch.cs
+++ Game/Assets/Scenes/StartMenu/Scripts/SceneSwitch.cs
@@ -19,20 +19,38 @@ public class SceneSwitch : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Should switch scene but also ask the cutscene manager to perchance do a cutscene
+    /// AKA load cutscene and next scene in that order, then unload itself
+    /// </summary>
+    /// <param name="sceneName"></param>
     public void LoadScene(string sceneName) {
 
-        string curr = SceneManager.GetActiveScene().name;
-        // SceneManager.LoadScene(sceneName);
-        StartCoroutine(SwitchScene(sceneName, curr));
+        StartCoroutine(SwitchScene(sceneName));
 
     }
 
-    IEnumerator SwitchScene(string final, string curr){
+    IEnumerator SwitchScene(string destination){
 
+        // var asyncLoad = SceneManager.LoadSceneAsync("Cutscene", LoadSceneMode.Additive);
+
+        string temp = "SCENES RN:\n";
+
+        for(int i = 0; true; i++){
+            try{
+                temp += $"[{i}] {SceneManager.GetSceneAt(i).name}\n";
+            } catch {
+                print(temp);
+                break;
+            }
+        }
+
+        // Add cutscene and next scene to load queue
+        
         SceneManager.LoadScene("Cutscene", LoadSceneMode.Additive);
-        SceneManager.LoadScene(final, LoadSceneMode.Additive);
+        SceneManager.LoadScene(destination, LoadSceneMode.Additive);
 
-        Scene nextScene = SceneManager.GetSceneByName("Cutscene");
+        /*
         int timeout = 200;
         while ((!nextScene.IsValid() || !nextScene.isLoaded) && timeout-- > 0)
             yield return null;
@@ -43,12 +61,21 @@ public class SceneSwitch : MonoBehaviour
         if(!nextScene.isLoaded)
             Debug.LogError("NOT LOADED");
 
-        if(nextScene.IsValid() && nextScene.isLoaded)
-            SceneManager.SetActiveScene(nextScene);
-        else
-            Debug.LogError("SwitchScene messed up.");
+        if(nextScene.IsValid() && nextScene.isLoaded) */
+        // while(!asyncLoad.isDone)
+
+        // Wait for cutscene to load
+        while(!SceneManager.GetSceneAt(1).isLoaded)
+            yield return null;
+
+        // Set cutscene to primary
+        SceneManager.SetActiveScene(SceneManager.GetSceneAt(1));
+        print($"NEW ACTIVE SCENE: {SceneManager.GetActiveScene().name}");
+        /* else
+            Debug.LogError("SwitchScene messed up."); */
         
-        SceneManager.UnloadSceneAsync(curr);
+        // Unload current scene
+        SceneManager.UnloadSceneAsync(0);
 
     }
 
