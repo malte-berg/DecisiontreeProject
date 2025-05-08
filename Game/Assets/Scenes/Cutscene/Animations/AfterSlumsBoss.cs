4534328fdf3f4163733562fd5a0a9a5fe8091654
Albert Falck <albert.falck1@gmail.com>
2025-05-08 11:40:48 +0200

    After Slums Boss Cutscene
    Added the cutscene that occurs after the player defeats the boss of the slums.
    Right now, the cutscene depends on the boss being a "Guard" from the Commoner's Quarters, but this is easily changed if and/or when we create a unique boss for the slums.

---
+++ Game/Assets/Scenes/Cutscene/Animations/AfterSlumsBoss.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using UnityEngine;
+
+public class AfterSlumsBoss : SceneScript
+{
+    public override IEnumerator RunAnimation() {
+
+        //"..." image is set.
+        bg.SetBG(4);
+
+        db.Enqueue(@"
+        Narrator§As Ynnos strikes down the last man, more guards storm into the Arena.
+        ");
+        db.Enqueue(@"
+        Ynnos§“Hah… Hah… I can’t fight anymore… Will this truly be the end?“
+        ");
+        db.Enqueue(@"
+        Narrator§Ynnos prepares his final stand, when suddenly a robed man shouts behind him.
+        ");
+
+        db.ContinueDialogue();
+        yield return WaitForDialogue();
+
+
+        //"..." image is set.
+        bg.SetBG(5);
+        yield return new WaitForSeconds(1);
+
+        db.Enqueue(@"
+        Robed Man§“Ynnos! Over here! I will help you escape this place!”
+        ");
+        db.Enqueue(@"
+        Narrator§Left with no choice but to trust this man, Ynnos runs towards him. They eventually exit the Arena through a hidden back entrance and end up in a hiding place, seemingly prepared beforehand by the robed man.
+        ");
+
+        db.ContinueDialogue();
+        yield return WaitForDialogue();
+
+
+        //
+        bg.SetBG(6);
+        yield return new WaitForSeconds(1);
+
+        db.Enqueue(@"
+        Narrator§As Ynnos catches his breath, the robed man gives him a map and a card.
+        ");
+        db.Enqueue(@"
+        Robed Man§“Me and my associates have arranged for you and your sister to live in an apartment in the Commoner’s Quarters.”
+        ");
+        db.Enqueue(@"
+        Robed Man§“That card will get you through the Slums Gate to access the Commoner’s Quarters, and the map will get you the rest of the way.”
+        ");
+        db.Enqueue(@"
+        Robed Man§“You’d better hurry Ynnos. The Argus’ guards will no doubt come after your house here in the Slums any minute and get your sister. Good luck.”
+        ");
+        db.Enqueue(@"
+        Ynnos§“Wait, wait, who are you?! How did you know all this would happen?”
+        ");
+        db.Enqueue(@"
+        Robed Man§“We are the Rebellion, and we will need your power to fight the overlord of this city. We’ll contact you later!”
+        ");
+        db.Enqueue(@"
+        Narrator§As the robed man disappeared, running into the night, Ynnos hurried to his home. He picked up his stuff and got his sister, and together they passed through the Slums Gate.
+        ");
+        
+
+        db.ContinueDialogue();
+        yield return WaitForDialogue();
+        yield return new WaitForSeconds(1);
+
+        //"commonersBackground" image is set.
+        bg.SetBG(7);
+        yield return new WaitForSeconds(1);
+
+        db.Enqueue(@"
+        Narrator§After arriving at the apartment and entering their new home, Ynnos find a letter on a table, addressed to him.
+        ");
+        db.Enqueue(@"
+        Contents of letter§<i>Fight in the Arena, Ynnos. We will meet you there, and all will be explained. Don’t die.   - Samuel</i>
+        ");
+
+        db.ContinueDialogue();
+        yield return WaitForDialogue();
+        yield return new WaitForSeconds(1);
+    }
+}
