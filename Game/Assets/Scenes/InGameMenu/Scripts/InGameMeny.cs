937cf227f33cd121b30f5223379ad13807d633fc
Viktor Sandström <visand@kth.se>
2025-05-08 14:22:16 +0200

    Combat counter in game menu

--- Game/Assets/Scenes/InGameMenu/Scripts/InGameMeny.cs
+++ Game/Assets/Scenes/InGameMenu/Scripts/InGameMeny.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,6 +14,18 @@ public class TEMP : MonoBehaviour{
         
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         player.HidePlayer();
+        TMP_Text combatCounter = transform.GetChild(0).GetChild(1).GetChild(0).GetChild(0).GetComponent<TMP_Text>();
+        combatCounter.text = $"{player.CombatsWon}";
+        float redness = (float)player.CombatsWon / 20;
+        combatCounter.faceColor = new Color(1,1-redness,1-redness);
+
+        if(player.CombatsWon > 10)
+            combatCounter.faceColor = new Color(0.7f,0.7f,0.7f);
+        else if(player.CombatsWon == 10) {
+            combatCounter.fontMaterial.EnableKeyword("UNDERLAY_ON");
+            combatCounter.fontMaterial.EnableKeyword("OUTLINE_ON");
+            combatCounter.faceColor = new Color(0.7f,0,0);
+        }
 
         // try to avoid unnecessary performance overhead
         if (currentAreaIndex != player.CurrentAreaIndex)
