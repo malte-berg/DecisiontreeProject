f1ccf6bd8208f657ecfb60c48e7a17125a1236f6
Viktor Sandström <visand@kth.se>
2025-05-14 14:17:39 +0200

    Fixed #186 in inventory

--- Game/Assets/Scenes/Inventory/Scripts/InventoryManager.cs
+++ Game/Assets/Scenes/Inventory/Scripts/InventoryManager.cs
@@ -19,6 +19,17 @@ public class InventoryManager : MonoBehaviour{
         player.transform.position = new Vector3(-5.3f, 1.8f, -1);
         player.SM.SetCharacter(player);
 
+        SpriteRenderer[] sr = new SpriteRenderer[7];
+        Transform container = player.transform.GetChild(0);
+
+        for(int i = 0; i < sr.Length; i++) {
+            sr[i] = container.GetChild(i).GetComponent<SpriteRenderer>();
+        }
+
+        foreach (SpriteRenderer s in sr) {
+            s.color = Color.white;
+        }
+
         il = GetComponentInChildren<InventoryList>();
         il.Init(this);
         sl = GetComponentInChildren<StatsList>();
