35f347acb07af728ab9ed6a54826f8c3e960c8fa
Malte Berg <malte@malte.nu>
2025-05-14 08:54:47 +0200

    Change UpdateBar to include changing bar color

--- Game/Assets/Scenes/Combat/Scripts/Bar.cs
+++ Game/Assets/Scenes/Combat/Scripts/Bar.cs
@@ -14,7 +14,7 @@ public class Bar : MonoBehaviour
         
     }
 
-    public void UpdateBar(int current, int max)
+    public void UpdateBar(int current, int max, int type)
     {
         if (slider == null || valueText == null || max == 0) return;
 
@@ -25,9 +25,25 @@ public class Bar : MonoBehaviour
         valueText.text = $"{current} / {max}";
         float colorValue = MapValue(current, 0f, max, 1.0f, 0f);
         valueText.color = new Color(colorValue, colorValue, colorValue, 1f);
+        if (type == 0) {
+            UpdateBarColor(percentage);
+        }
     }
 
     private float MapValue(float value, float fromLow, float fromHigh, float toLow, float toHigh) {
         return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+    }
+
+    private void UpdateBarColor(float hpPercentage) {
+        Image sliderFill = slider.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>();
+        if (hpPercentage >= 0.75f) {
+            sliderFill.color = new Color32(0, 255, 0, 255);
+        } else if (hpPercentage >= 0.50f) {
+            sliderFill.color = new Color32(245, 236, 66, 255);
+        } else if (hpPercentage >= 0.25f) {
+            sliderFill.color = new Color32(245, 144, 66, 255);
+        } else {
+            sliderFill.color = new Color32(255, 43, 43, 255);
+        }
     }
 }
